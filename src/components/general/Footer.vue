7e2d6b959f797d9b9cfbee3b4e63d1cf24a96784
wildone <max.barrass@gmail.com>
2023-05-09 13:06:49 +1000

    add policies section.

--- src/components/general/Footer.vue
+++ src/components/general/Footer.vue
@@ -37,16 +37,26 @@
       <div
         class="footer__bottom-right d-none d-md-flex justify-content-md-start justify-content-between"
       >
-        <!-- <ul class="footer__bottom-list p-0">
-          <li class="footer__bottom-item">PHILOSOPHY</li>
-          <li class="footer__bottom-item">APPROACH</li>
-          <li class="footer__bottom-item">BLOG</li>
-        </ul> -->
-        <ul class="footer__bottom-list p-0 ps-5 ms-5">
-          <!-- <li class="footer__bottom-item">PRODUCTS</li>
-          <li class="footer__bottom-item">TEAM</li> -->
-          <li class="footer__bottom-item"><a href="https://www.linkedin.com/company/anchora-consulting/">LINKEDIN</a></li>
-        </ul>
+        <div>
+          <p class="footer__bottom-title mb-0">Policies</p>
+          <p class="footer__bottom-subtitle">
+            <ul class="footer__bottom-list p-0">
+              <li class="footer__bottom-item"><a href="@/assets/policies/Modern-Slavery-Policy.pdf">Modern Slavery Policy</a></li>
+            </ul>
+          </p>
+        </div>
+      </div>
+      <div
+        class="footer__bottom-right d-none d-md-flex justify-content-md-start justify-content-between"
+      >
+        <div>
+          <p class="footer__bottom-title mb-0">Social</p>
+          <p class="footer__bottom-subtitle">
+            <ul class="footer__bottom-list p-0">
+              <li class="footer__bottom-item"><a href="https://www.linkedin.com/company/anchora-consulting/">LINKEDIN</a></li>
+            </ul>
+          </p>
+        </div>
       </div>
       <p class="mb-0 footer__top-description d-block d-md-none my-4">
         Strategic Marketing Technology <br />
